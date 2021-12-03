5888ebf8d302ad2dbf4a6bda1271d62ac32da62b
alpharameeztech <rameezisrarulhaq@gmail.com>
2021-12-03 14:15:47 +0500

    Update the web routes to direct traffic to call the Home controller method

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',  [HomeController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard',  [HomeController::class, 'index'])->name('dashboard');
 
 require __DIR__.'/auth.php';
